baacb2facbb9b7a80dd514f9917482181a47c9e7
mathewsageorge <mathewsgeorge202@gmail.com>
2024-06-20 01:03:54 +0530

    test finalll

--- server.js
+++ server.js
@@ -152,9 +152,19 @@ app.post('/start-class', async (req, res) => {
 
         await Promise.all(Object.keys(serialEmails).map(async (serial) => {
             if (!readSerialNumbers[serial] && serialEmails[serial].class === classIdentifier) {
-                const emailText = `Dear ${serialEmails[serial].name},\n\nAlert From NFCAMS-CEK: You were marked absent for ${subject} on ${currentDate}, during ${period}.`;
+                const emailText = `Dear ${serialEmails[serial].name},\n\nAlert From NFCAMS: You were marked absent for ${subject} on ${currentDate}, during ${period}.`;
                 await sendEmail(serialEmails[serial].email, "NFCAMS-Absence Notification", emailText);
                 absenteesNotified++;
+
+                // Save absentee details to the database
+                const absenteeRecord = new recordModel({
+                    serialNumber: serial,
+                    logData: "Absent",
+                    time: currentDate,
+                    period: period,
+                    subject: subject,
+                });
+                await absenteeRecord.save();
             }
         }));
 
